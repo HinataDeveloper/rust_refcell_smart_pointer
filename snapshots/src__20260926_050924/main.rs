148ce1779399ddc33fd74e9cdfe6edae21b0c0fb
Hinata <StubbornStruggle@gmail.com>
2026-09-26 05:09:51 +0330

    Using RecCell Smart Pointer : Having mutable and multi owner with combine Rc<T> and RefCell<T>

---
+++ snapshots/src__20260926_050924/main.rs
@@ -0,0 +1,57 @@
+// main.rs
+// Date: Thu Sep 26 2026
+// Educational and Practice Rust Programming Language Code
+
+// Project: Learning Chapter 15
+// Goal: Using RecCell Smart Pointer
+// Dependency: Without dependency
+
+// rustc 1.100.0-nightly (f7575a9da 2026-09-24)
+// binary: rustc
+// commit-hash: f7575a9da8e4a4fca3b5668d5a2ea7476db44b3f
+// commit-date: 2026-09-24
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.1
+
+// cargo 1.100.0-nightly (98a09e7e7 2026-09-21)
+// release: 1.100.0-nightly
+// commit-hash: 98a09e7e7d62850f14e5b6132101fc1edd19a16f
+// commit-date: 2026-09-21
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Fedora 44.0.0 [64-bit]
+
+// Kernel Version: 7.2.7-200.fc44.x86_64
+// Firmware Version: 71CN51WW(V1.21)
+
+use std::{cell::RefCell, rc::Rc};
+
+use ref_cell_smart_pointer::List::{Cons, Nil};
+
+fn main() {
+    println!("\n");
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    println!("a content before: {:?}", a);
+    println!("b content before: {:?}", b);
+    println!("c content before: {:?}", c);
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    *value.borrow_mut() += 391;
+
+    println!("a content before: {:?}", a);
+    println!("b content before: {:?}", b);
+    println!("c content before: {:?}", c);
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    println!("\nThe End ...\n");
+}
